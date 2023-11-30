9d13976e09f3d2330fd3bb2bca973124ccd65b05
Chopen <75852177+diouri844@users.noreply.github.com>
2023-11-30 18:44:03 +0100

    Setup chi router & chi middelware

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 // define the server handler function : 
@@ -14,12 +16,18 @@ func basicHandler(w http.ResponseWriter, r *http.Request){
 
 
 func  main()  {
+	// seetup new router by chi :
+	router := chi.NewRouter()
+	// setup logger middleware for the router:
+	router.Use(middleware.Logger)
+	// use defined router to setup endpoint:
+	router.Get("/Ping", basicHandler)
 	// setup an http server :
 	server := &http.Server{
 		// define the http server port :
 		Addr: ":3000",
 		// define the http server handler :
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 	// run the server:
 	err := server.ListenAndServe()
