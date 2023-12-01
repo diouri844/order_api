da6fdcbfc8abdcded9546f19a337298c3b568f6f
Chopen <75852177+diouri844@users.noreply.github.com>
2023-12-01 18:01:47 +0100

    update server entry point

--- main.go
+++ main.go
@@ -2,38 +2,21 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"context"
+	"github.com/diouri844/order_api/application"
 )
 
-// define the server handler function : 
-
-func basicHandler(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("Go Server under runing now ..."))
-}
 
 
 
 func  main()  {
-	// seetup new router by chi :
-	router := chi.NewRouter()
-	// setup logger middleware for the router:
-	router.Use(middleware.Logger)
-	// use defined router to setup endpoint:
-	router.Get("/Ping", basicHandler)
-	// setup an http server :
-	server := &http.Server{
-		// define the http server port :
-		Addr: ":3000",
-		// define the http server handler :
-		Handler: router,
-	}
-	// run the server:
-	err := server.ListenAndServe()
-	// check if there was an error :
-	if err != nil {
-		fmt.Println("Failed to Runing Server: ", err)
+	// define my app :
+	server := application.New()
+	// start the server ( app ) with defined start method 
+	// and check if there is any error :
+	erro := server.Start(context.TODO())
+	if erro != nil {
+		fmt.Println("Failed to Runing Server: ", erro)
 	}
 }
 
