707a88679bfe91a47b762642891d0d8f8a9fa7b1
Chopen <75852177+diouri844@users.noreply.github.com>
2023-12-20 20:53:03 +0100

    Update app go file

--- application/app.go
+++ application/app.go
@@ -4,49 +4,56 @@ package application
 import (
 	"net/http"
 	"context"
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
 	"fmt"
-	"github.com/redis/go-redis/v9"
 )
 
 // define the app structur :
 type App struct {
 	router http.Handler
 	// add the redis clinet to the app type :
-	rdb *redis.Client
+	rdb *sql.DB
 }
 
 
 
 // define the constructoor of the app :
 
-
 func New() *App {
+	// try to open a connection to the database :
+	db, err := sql.Open("sqlite3", "../OrderDatabase.db")
+	// check error :
+	if err != nil {
+		fmt.Println( err )
+		fmt.Println("Faild to open database")
+	}
+	if err = createTables(db); err != nil {
+		fmt.Println(err)
+		panic("Failed to create tables")
+	}
+	// setup order table if not already present :
 	app := &App{
 		// setup app routs :
 		router: loadRoutes(),
 		// setup app reids client:
-		rdb: redis.NewClient(&redis.Options{}),
+		rdb: db,
 	}
 	return app
 }
 
 // define a start method :
 func (app *App) Start(ctx context.Context ) error {
+	// test ping :
 	server := &http.Server{
 		// define the http server port :
 		Addr: ":3000",
 		// define the http server handler :
 		Handler: app.router,
 	}
-	// ping to the redis running instance :
-	err := app.rdb.Ping(ctx).Err()
-	// check if the connection with redis is failed:
-	if err != nil {
-		return fmt.Errorf("Failed to Connect redis: %w", err)
-	}
 	// run the server:
 	fmt.Println(":: Staring Server on Dev mode ....")
-	err = server.ListenAndServe()
+	err := server.ListenAndServe()
 	// check if there was an error :
 	if err != nil {
 		return fmt.Errorf("Failed to Runing Server: %w",err)
@@ -54,3 +61,22 @@ func (app *App) Start(ctx context.Context ) error {
 	return nil
 }
 
+
+// define setup tables for the database :
+// createTables creates necessary tables in the SQLite database
+func createTables(db *sql.DB) error {
+	// Create 'orders' table with UUID as the data type for the 'id' column
+	_, err := db.Exec(`
+   CREATE TABLE IF NOT EXISTS orders (
+      id TEXT PRIMARY KEY,
+      customer_id INTEGER NOT NULL,
+      product_id INTEGER NOT NULL,
+      quantity INTEGER NOT NULL,
+      order_date DATETIME NOT NULL,
+      )
+	`)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return err
+}
